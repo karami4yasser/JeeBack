860f81f9546ea36578bbe950b57ae4b1d4b67633
YASSER KARAMI <83478271+karami4yasser@users.noreply.github.com>
2022-09-12 16:17:16 +0100

    ok

--- src/main/java/com/example/demo/repository/FournisseurRepository.java
+++ src/main/java/com/example/demo/repository/FournisseurRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Founisseur;
-
+import java.util.List;
 public interface FournisseurRepository  extends JpaRepository<Founisseur, Long>{
 
     List<Founisseur> findAll();
